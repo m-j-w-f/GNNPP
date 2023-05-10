810f9d1cd2b483b4dffe1a74f53188940692ed0c
Moritz <>
2023-05-10 19:44:46 +0200

    🧪 Added Experiments

--- gnn/README.md
+++ gnn/README.md
@@ -1,3 +1,8 @@
 # 🧪 Graph Neural Network Experiments
 
 Files in this folder are used to perform experiments with GNNs to evaluate which architectures are promising.
+
+## 🚧 Current Problems
+
+    - How to construct the Graph
+    - What about Stations that are far outside (isolated Nodes)?
--- utils/helpers.py
+++ utils/helpers.py
@@ -1,15 +1,17 @@
 import pandas as pd
 import os
-def  load_data() -> pd.DataFrame:
+def  load_data(indexed:bool = True) -> pd.DataFrame:
     """
     Loads the data from the feather file and converts the station column to an integer.
     :return: Dataframe with the data.
     """
     path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data_RL18.feather'))
     df = pd.read_feather(path)
-    df.station = pd.to_numeric(df.station, downcast='integer') # convert station to integer
-    df.index = df.date # add DatetimeIndex
-    df.index = df.index.tz_convert(None) # remove timezone
+    df.station = pd.to_numeric(df.station, downcast='integer') - 1 # convert station to integer and subtract 1 to make it 0-based
+    df = df.sort_values(by=['date','station']) # sort by date and station
+    if indexed:
+        df.index = df.date # add DatetimeIndex
+        df.index = df.index.tz_convert(None) # remove timezone
     return df
 
 def load_stations(df: pd.DataFrame = None) -> pd.DataFrame:
